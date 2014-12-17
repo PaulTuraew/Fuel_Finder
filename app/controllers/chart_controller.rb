77e02e39e3edf4f0e0c5bbbf9b3ac85c4e544be6
paulturaew <paul.turaew@gmail.com>
2014-12-17 11:59:32 -0500

    all better now

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -1,5 +1,5 @@
 class ChartController < ApplicationController
-  include ChartHelper
+  #include ChartHelper
   def index
   end
 
@@ -24,81 +24,82 @@ class ChartController < ApplicationController
     end
   end
 
-  def chart_from_station_counts
-    hostname  = "http://developer.nrel.gov"
-    api_key   = "oyz3TmTuOLaY0QS8hpBNk32s4nyocBlVasRABgbN"
-    state     = params[:state]
-    limit     = 1
-    data_type = "alt-fuel-stations"
+#no longer needed to make calls to API each time
+  # def chart_from_station_counts
+  #   hostname  = "http://developer.nrel.gov"
+  #   api_key   = "oyz3TmTuOLaY0QS8hpBNk32s4nyocBlVasRABgbN"
+  #   state     = params[:state]
+  #   limit     = 1
+  #   data_type = "alt-fuel-stations"
 
-    #abstracting out the URI, res encodes string to URI (decrease errors), grab data from URI and parse what is returned into a hash
-    uri = URI("#{hostname}/api/#{data_type}/v1.json?api_key=#{api_key}&state=#{state}&limit=#{limit}")
-    res = Net::HTTP.get(uri)
-    json = JSON.parse(res)
+  #   #abstracting out the URI, res encodes string to URI (decrease errors), grab data from URI and parse what is returned into a hash
+  #   uri = URI("#{hostname}/api/#{data_type}/v1.json?api_key=#{api_key}&state=#{state}&limit=#{limit}")
+  #   res = Net::HTTP.get(uri)
+  #   json = JSON.parse(res)
 
-    # create empty data array
-    data = []
+  #   # create empty data array
+  #   data = []
 
-    # get total station count from json
-    station_count = json['station_counts']['total']
+  #   # get total station count from json
+  #   station_count = json['station_counts']['total']
 
-    # iterate through each station count and get percentage of total stations
-    json['station_counts']['fuels'].each do |key, value|
-      fuel_type = key
-      # get total key from hash
-      count = value['total']
-      # calculate percentage as float
-      percent = count / station_count.to_f * 100
-      # store in data array
-      data << [get_fuel_type_desc(fuel_type), percent]
-    end
+  #   # iterate through each station count and get percentage of total stations
+  #   json['station_counts']['fuels'].each do |key, value|
+  #     fuel_type = key
+  #     # get total key from hash
+  #     count = value['total']
+  #     # calculate percentage as float
+  #     percent = count / station_count.to_f * 100
+  #     # store in data array
+  #     data << [get_fuel_type_desc(fuel_type), percent]
+  #   end
 
-    # return array as json
-    respond_to do |format|
-      format.json {render json: data} #this is data returned to user
-    end
-  end
+  #   # return array as json
+  #   respond_to do |format|
+  #     format.json {render json: data} #this is data returned to user
+  #   end
+  # end
 
   # method used to make data calls directly from API - hitting the fuel_stations key and calculating %
   # passing a state and getting back a hash with the data
-  def chart_from_stations
-    hostname  = "http://developer.nrel.gov"
-    api_key   = "oyz3TmTuOLaY0QS8hpBNk32s4nyocBlVasRABgbN"
-    state     = params[:state]
-    limit     = 10
-    data_type = "alt-fuel-stations"
+  # def chart_from_stations
+  #   hostname  = "http://developer.nrel.gov"
+  #   api_key   = "oyz3TmTuOLaY0QS8hpBNk32s4nyocBlVasRABgbN"
+  #   state     = params[:state]
+  #   limit     = 10
+  #   data_type = "alt-fuel-stations"
 
-    #abstracting out the URI, res encodes string to URI (decrease errors), grab data from URI and parse what is returned into a hash
-    uri = URI("#{hostname}/api/#{data_type}/v1.json?api_key=#{api_key}&state=#{state}&limit=#{limit}")
-    res = Net::HTTP.get(uri)
-    json = JSON.parse(res)
+  #   #abstracting out the URI, res encodes string to URI (decrease errors), grab data from URI and parse what is returned into a hash
+  #   uri = URI("#{hostname}/api/#{data_type}/v1.json?api_key=#{api_key}&state=#{state}&limit=#{limit}")
+  #   res = Net::HTTP.get(uri)
+  #   json = JSON.parse(res)
 
-    # create empty stations hash
-    stations = {}
+  #   # create empty stations hash
+  #   stations = {}
 
-    # populate stations hash with id and fuel type code
-    json['fuel_stations'].each do |data|
-      id = data['id']
-      code = data['fuel_type_code']
-      stations[id] = code
-    end
+  #   # populate stations hash with id and fuel type code
+  #   json['fuel_stations'].each do |data|
+  #     id = data['id']
+  #     code = data['fuel_type_code']
+  #     stations[id] = code
+  #   end
 
-    # group stations by fuel type code
-    grouped = stations.group_by { |key, value| value }
+  #   # group stations by fuel type code
+  #   grouped = stations.group_by { |key, value| value }
 
-    # massage data to fit expected highcharts format
-    # array of arrays, ex:
-    # [["name", percent], ["name", percent], ["name", percent]]
-    data = []
-    grouped.each do |key, value|
-      percent = value.count / grouped.count.to_f * 100
-      data << [key, percent]
-    end
+  #   # massage data to fit expected highcharts format
+  #   # array of arrays, ex:
+  #   # [["name", percent], ["name", percent], ["name", percent]]
+  #   data = []
+  #   grouped.each do |key, value|
+  #     percent = value.count / grouped.count.to_f * 100
+  #     data << [key, percent]
+  #   end
 
-    # return array as json
-    respond_to do |format|
-      format.json {render json: data} #this is data returned to user
-    end
-  end
+  #   # return array as json
+  #   respond_to do |format|
+  #     format.json {render json: data} #this is data returned to user
+  #   end
+  # end
 
 end
--- app/helpers/chart_helper.rb
+++ app/helpers/chart_helper.rb
@@ -1,77 +1,77 @@
-#No longer needed now that data is in db
-# module ChartHelper
-#   FUEL_TYPES = {
-#     "BD"   => "Biodiesel (B20 and above)",
-#     "CNG"  => "Compressed Natural Gas",
-#     "E85"  => "Ethanol (E85)",
-#     "ELEC" => "Electric",
-#     "HY"   => "Hydrogen",
-#     "LNG"  => "Liquefied Natural Gas",
-#     "LPG"  => "Liquefied Petroleum Gas (Propane)",
-#   }
+# No longer needed now that data is in db
+module ChartHelper
+  FUEL_TYPES = {
+    "BD"   => "Biodiesel (B20 and above)",
+    "CNG"  => "Compressed Natural Gas",
+    "E85"  => "Ethanol (E85)",
+    "ELEC" => "Electric",
+    "HY"   => "Hydrogen",
+    "LNG"  => "Liquefied Natural Gas",
+    "LPG"  => "Liquefied Petroleum Gas (Propane)",
+  }
 
 # No longer needed now that data is in db
 # accessed in index.html.erb (views) for the dropdown menu http://stackoverflow.com/questions/6400010/rails-select-drop-down-for-states
-  # def us_states
-  #   [
-  #     ['Alabama', 'AL'],
-  #     ['Alaska', 'AK'],
-  #     ['Arizona', 'AZ'],
-  #     ['Arkansas', 'AR'],
-  #     ['California', 'CA'],
-  #     ['Colorado', 'CO'],
-  #     ['Connecticut', 'CT'],
-  #     ['Delaware', 'DE'],
-  #     ['District of Columbia', 'DC'],
-  #     ['Florida', 'FL'],
-  #     ['Georgia', 'GA'],
-  #     ['Hawaii', 'HI'],
-  #     ['Idaho', 'ID'],
-  #     ['Illinois', 'IL'],
-  #     ['Indiana', 'IN'],
-  #     ['Iowa', 'IA'],
-  #     ['Kansas', 'KS'],
-  #     ['Kentucky', 'KY'],
-  #     ['Louisiana', 'LA'],
-  #     ['Maine', 'ME'],
-  #     ['Maryland', 'MD'],
-  #     ['Massachusetts', 'MA'],
-  #     ['Michigan', 'MI'],
-  #     ['Minnesota', 'MN'],
-  #     ['Mississippi', 'MS'],
-  #     ['Missouri', 'MO'],
-  #     ['Montana', 'MT'],
-  #     ['Nebraska', 'NE'],
-  #     ['Nevada', 'NV'],
-  #     ['New Hampshire', 'NH'],
-  #     ['New Jersey', 'NJ'],
-  #     ['New Mexico', 'NM'],
-  #     ['New York', 'NY'],
-  #     ['North Carolina', 'NC'],
-  #     ['North Dakota', 'ND'],
-  #     ['Ohio', 'OH'],
-  #     ['Oklahoma', 'OK'],
-  #     ['Oregon', 'OR'],
-  #     ['Pennsylvania', 'PA'],
-  #     ['Puerto Rico', 'PR'],
-  #     ['Rhode Island', 'RI'],
-  #     ['South Carolina', 'SC'],
-  #     ['South Dakota', 'SD'],
-  #     ['Tennessee', 'TN'],
-  #     ['Texas', 'TX'],
-  #     ['Utah', 'UT'],
-  #     ['Vermont', 'VT'],
-  #     ['Virginia', 'VA'],
-  #     ['Washington', 'WA'],
-  #     ['West Virginia', 'WV'],
-  #     ['Wisconsin', 'WI'],
-  #     ['Wyoming', 'WY']
-  #   ]
-  # end
+  def us_states
+    [
+      ['Alabama', 'AL'],
+      ['Alaska', 'AK'],
+      ['Arizona', 'AZ'],
+      ['Arkansas', 'AR'],
+      ['California', 'CA'],
+      ['Colorado', 'CO'],
+      ['Connecticut', 'CT'],
+      ['Delaware', 'DE'],
+      ['District of Columbia', 'DC'],
+      ['Florida', 'FL'],
+      ['Georgia', 'GA'],
+      ['Hawaii', 'HI'],
+      ['Idaho', 'ID'],
+      ['Illinois', 'IL'],
+      ['Indiana', 'IN'],
+      ['Iowa', 'IA'],
+      ['Kansas', 'KS'],
+      ['Kentucky', 'KY'],
+      ['Louisiana', 'LA'],
+      ['Maine', 'ME'],
+      ['Maryland', 'MD'],
+      ['Massachusetts', 'MA'],
+      ['Michigan', 'MI'],
+      ['Minnesota', 'MN'],
+      ['Mississippi', 'MS'],
+      ['Missouri', 'MO'],
+      ['Montana', 'MT'],
+      ['Nebraska', 'NE'],
+      ['Nevada', 'NV'],
+      ['New Hampshire', 'NH'],
+      ['New Jersey', 'NJ'],
+      ['New Mexico', 'NM'],
+      ['New York', 'NY'],
+      ['North Carolina', 'NC'],
+      ['North Dakota', 'ND'],
+      ['Ohio', 'OH'],
+      ['Oklahoma', 'OK'],
+      ['Oregon', 'OR'],
+      ['Pennsylvania', 'PA'],
+      ['Puerto Rico', 'PR'],
+      ['Rhode Island', 'RI'],
+      ['South Carolina', 'SC'],
+      ['South Dakota', 'SD'],
+      ['Tennessee', 'TN'],
+      ['Texas', 'TX'],
+      ['Utah', 'UT'],
+      ['Vermont', 'VT'],
+      ['Virginia', 'VA'],
+      ['Washington', 'WA'],
+      ['West Virginia', 'WV'],
+      ['Wisconsin', 'WI'],
+      ['Wyoming', 'WY']
+    ]
+  end
 
-#No longer needed
-#   def get_fuel_type_desc(abbr)
-#     FUEL_TYPES[abbr]
-#   end
+# No longer needed
+  def get_fuel_type_desc(abbr)
+    FUEL_TYPES[abbr]
+  end
 
-# end
+end
